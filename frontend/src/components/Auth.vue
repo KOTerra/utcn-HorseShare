18ad55e247f52be7b5fed83548cc28896cfdaf28
Stoica Mihai <82443072+KOTerra@users.noreply.github.com>
2025-10-22 10:20:52 +0300

    moved initial location handling

--- frontend/src/components/Auth.vue
+++ frontend/src/components/Auth.vue
@@ -3,15 +3,9 @@
     <div class="bg-white shadow-xl rounded-2xl p-8 w-80 text-center">
       <h1 class="text-2xl font-bold mb-6">Login</h1>
 
-      <button
-        @click="loginWithGoogle"
-        class="flex items-center justify-center gap-3 bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg w-full transition"
-      >
-        <img
-          src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
-          alt="Google"
-          class="w-5 h-5"
-        />
+      <button @click="loginWithGoogle"
+        class="flex items-center justify-center gap-3 bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg w-full transition">
+        <img src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google" class="w-5 h-5" />
         Login with Google
       </button>
 
@@ -27,12 +21,10 @@ import { ref } from "vue"
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 import { auth } from "../firebase"
 import { userStore } from "../stores/userStores.js"
+import { useUserLocation } from '../composables/useUserLocation.js'
 
-const DEFAULT_LAT = 46.77 // Cluj-Napoca
-const DEFAULT_LON = 23.59
 const userEmail = ref("")
 const loginError = ref(null)
-const locationError = ref(null)
 
 const API_URL = import.meta.env.VITE_API_URL // Read from .env
 
@@ -47,6 +39,7 @@ const loginWithGoogle = async () => {
     console.log("Logged in as:", user.email)
 
     // Get user's location or fallback
+    const { locationError, getUserLocationAsync } = useUserLocation()
     const location = await getUserLocationAsync()
 
     // Use the env API URL
@@ -70,31 +63,6 @@ const loginWithGoogle = async () => {
   }
 }
 
-function getUserLocationAsync() {
-  loginError.value = null
-  locationError.value = null
-
-  return new Promise((resolve) => {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords
-          console.log("Location found:", latitude, longitude)
-          resolve([latitude, longitude])
-        },
-        (error) => {
-          console.warn("Geolocation error:", error.message)
-          locationError.value = "Geolocation failed. Using default location."
-          resolve([DEFAULT_LAT, DEFAULT_LON])
-        },
-        { timeout: 5000 }
-      )
-    } else {
-      locationError.value = "Geolocation not supported by your browser!"
-      resolve([DEFAULT_LAT, DEFAULT_LON])
-    }
-  })
-}
 </script>
 
 <style scoped>
