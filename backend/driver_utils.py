c9d519a7e84bd7d99d00ed29353c2a31069c0e83
stolniceanuiustin <stolniceanuiustin@gmail.com>
2025-11-06 11:17:26 +0200

    Made it so only drivers in range appear

--- backend/driver_utils.py
+++ backend/driver_utils.py
@@ -5,13 +5,13 @@ def compute_drivers_in_range(all_drivers_data, lat, lon, range):
     drivers_in_range = []
 
     for driver_data in all_drivers_data.values():
-        #if driver_data.get("loggedIn") == True:
-        location = driver_data.get("location")
+        if driver_data.get("loggedIn") == True:
+            location = driver_data.get("location")
 
-        driver_lat = location[0]
-        driver_lon = location[1]
+            driver_lat = location[0]
+            driver_lon = location[1]
 
-        if is_in_range(driver_lat, driver_lon, lat, lon, range): 
-            drivers_in_range.append(driver_data)
+            if is_in_range(driver_lat, driver_lon, lat, lon, range):
+                drivers_in_range.append(driver_data)
 
     return drivers_in_range
